96c05b3d77c84ed5df8b891881d5afcb8c5ec04f
PankajAjmera1 <coderajmera@gmail.com>
2024-08-15 20:11:55 +0530

    django admin

---
+++ djangocode/chaiaurdjango/chaiaurdjango/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+   # return HttpResponse("Hello, Django!")   
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse("About Django!")
---
+++ djangocode/reame.md
@@ -0,0 +1,16 @@
+##Commands
+
+```
+pip install uv
+uv venv
+.venv\Scripts\activate
+pip install Django
+django-admin startproject projectName
+cd Projectname
+python manage.py runserver
+```
+
+
+```
+ python manage.py startapp newchai
+ ```
